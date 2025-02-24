e5dfbdacccc0308956e71e893de94d7d511fa256
Omar Selim <ommarselim@gmail.com>
2025-02-25 01:11:25 +0200

    seeding villas to db

---
+++ WhiteLagoon.Infrastructure/Migrations/20250224230448_seedVillaToDb.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedVillaToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Created_Date", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "Updated_Date" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2025, 2, 25, 1, 4, 48, 460, DateTimeKind.Local).AddTicks(6360), "Villa 1 Description", "https://via.placeholder.com/150", "Villa 1", 6, 1000.0, 2000, new DateTime(2025, 2, 25, 1, 4, 48, 463, DateTimeKind.Local).AddTicks(3711) },
+                    { 2, new DateTime(2025, 2, 25, 1, 4, 48, 463, DateTimeKind.Local).AddTicks(4057), "Villa 2 Description", "https://via.placeholder.com/150", "Villa 2", 8, 2000.0, 3000, new DateTime(2025, 2, 25, 1, 4, 48, 463, DateTimeKind.Local).AddTicks(4066) },
+                    { 3, new DateTime(2025, 2, 25, 1, 4, 48, 463, DateTimeKind.Local).AddTicks(4069), "Villa 3 Description", "https://via.placeholder.com/150", "Villa 3", 10, 3000.0, 4000, new DateTime(2025, 2, 25, 1, 4, 48, 463, DateTimeKind.Local).AddTicks(4071) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
---
+++ WhiteLagoon.Infrastructure/Migrations/20250224230914_SeedVillaToDb2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedVillaToDb2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Created_Date", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "Updated_Date" },
+                values: new object[] { null, "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x400", "Royal Villa", 4, 200.0, 550, null });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Created_Date", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "Updated_Date" },
+                values: new object[] { null, "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x401", "Premium Pool Villa", 4, 300.0, 550, null });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Created_Date", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "Updated_Date" },
+                values: new object[] { null, "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x402", "Luxury Pool Villa", 4, 400.0, 750, null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Created_Date", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "Updated_Date" },
+                values: new object[] { new DateTime(2025, 2, 25, 1, 4, 48, 460, DateTimeKind.Local).AddTicks(6360), "Villa 1 Description", "https://via.placeholder.com/150", "Villa 1", 6, 1000.0, 2000, new DateTime(2025, 2, 25, 1, 4, 48, 463, DateTimeKind.Local).AddTicks(3711) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Created_Date", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "Updated_Date" },
+                values: new object[] { new DateTime(2025, 2, 25, 1, 4, 48, 463, DateTimeKind.Local).AddTicks(4057), "Villa 2 Description", "https://via.placeholder.com/150", "Villa 2", 8, 2000.0, 3000, new DateTime(2025, 2, 25, 1, 4, 48, 463, DateTimeKind.Local).AddTicks(4066) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Created_Date", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "Updated_Date" },
+                values: new object[] { new DateTime(2025, 2, 25, 1, 4, 48, 463, DateTimeKind.Local).AddTicks(4069), "Villa 3 Description", "https://via.placeholder.com/150", "Villa 3", 10, 3000.0, 4000, new DateTime(2025, 2, 25, 1, 4, 48, 463, DateTimeKind.Local).AddTicks(4071) });
+        }
+    }
+}
